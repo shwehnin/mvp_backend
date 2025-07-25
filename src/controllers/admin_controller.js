6ed3293c9a6ebc71327f3e8a2a67428f5617a210
shwehnin <hninhnin6398@gmail.com>
2025-07-25 19:06:05 +0700

    add pagination for users and groups buy admin

--- src/controllers/admin_controller.js
+++ src/controllers/admin_controller.js
@@ -2,9 +2,13 @@ const db = require("../models/user_model");
 const dbGroupBuy = require("../models/group_buy_model");
 const dbHDB = require("../models/hdb_model");
 const { success } = require("../utils/response");
+const { skipCount, limitCount } = require("../utils/helper");
+
 const getAllUsers = async (req, res, next) => {
     try {
-        const users = await db.find().select('-password -otp -resetOtp');
+        const skip = skipCount(req);
+        const limit = limitCount(req);
+        const users = await db.find().select('-password -otp -resetOtp').skip(skip).limit(limit);
         success(res, { message: "Get all users", data: users });
     } catch (error) {
         next(error);
@@ -13,9 +17,11 @@ const getAllUsers = async (req, res, next) => {
 
 const getAllGroupBuys = async (req, res, next) => {
     try {
+        const skip = skipCount(req);
+        const limit = limitCount(req);
         const groupBuys = await dbGroupBuy.find()
             .populate('organizer', 'name email')
-            .populate('participants.user', 'name email');
+            .populate('participants.user', 'name email').skip(skip).limit(limit);
         success(res, { message: "Group buy users", data: groupBuys });
     } catch (error) {
         next(error);
